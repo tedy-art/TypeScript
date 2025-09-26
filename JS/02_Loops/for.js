1d1f403ad9637b458ebc05d73588d3ce8a7414ec
Tejas Falke <tejasf4@gmail.com>
2025-09-26 16:39:02 +0530

    For

--- JS/02_Loops/for.js
+++ JS/02_Loops/for.js
@@ -1,4 +1,12 @@
+console.log("0 to 5 : ")
 for(let i = 0; i < 5; i++)
 {
     console.log(i);
+}
+
+console.log(" ")
+console.log("0 to 10 : ")
+for(let j = 0; j < 10; j++)
+{
+    console.log(j);
 }
