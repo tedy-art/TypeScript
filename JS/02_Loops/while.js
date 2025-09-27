9373c035266eb9a9e0a259b9a7d13e9c3b7aa608
Tejas Falke <tejasf4@gmail.com>
2025-09-27 08:49:14 +0530

    While Looping

--- JS/02_Loops/while.js
+++ JS/02_Loops/while.js
@@ -4,3 +4,19 @@ while(i<=5)
     console.log(i);
     i++;
 }
+
+let a = 1;
+while(a <= 10)
+{
+    console.log("Hi times : ",a);
+    a++;
+}
+
+let sum = 0;
+let num = 1;
+
+while(sum < 20){
+    sum += num;
+    num++;
+}
+console.log("sum of 0 to 20 is : ",sum);
